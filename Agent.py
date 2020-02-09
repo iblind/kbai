b901ab719ca9f74aa14448c25d6700c0be53b918
IB <ib2342@columbia.edu>
2020-02-09 00:11:32 -0500

    b02 done but brittle

--- Agent.py
+++ Agent.py
@@ -120,8 +120,38 @@ def check_if_object_inside_another(figure):
 
         return inside_present;
 
-def get_inside_object(figure):
-    print(figure)
+def get_inside_object_two_obj_total(figure):
+    for obj in figure:
+        if 'inside' in obj['properties'].keys():
+            return obj
+        else:
+            continue
+
+def get_outside_object_two_obj_total(figure):
+    for obj in figure:
+        if 'inside' in obj['properties'].keys():
+            continue
+        else:
+            return obj
+
+def compare_same_property_object(obj1, obj2):
+    differences ={}
+    # print(obj1)
+    # print(obj2)
+    for key in obj1['properties']:
+        try:
+            if obj1['properties'][key] == obj2['properties'][key]:
+                same_properties = 1
+                differences[key] = {'same_properties': same_properties, 'differences':''}
+            else:
+                same_properties = 0
+                differences[key] = {'same_properties':same_properties,'differences':{'A': obj1['properties'][key], 'B': obj2['properties'][key]}}
+        except:
+            differences = False
+
+    return differences
+    # print(differences)
+    # return differences
 
 class Agent:
     # The default constructor for your Agent. Make sure to execute any
@@ -148,7 +178,7 @@ class Agent:
         solvethings=1
         # solvethings
     # 'Basic Problem B-04' in problem.name
-        if('Basic Problem B-02' in problem.name): #TODO add other problems
+        if(solvethings): #TODO add other problems
 
             def handle_goal(x,differences):
 
@@ -194,6 +224,44 @@ class Agent:
                         if(differences_A_B==relevant_differences_C_and_choice):
                             returned_answer = int((answer[0]['figure']))
                             return returned_answer
+
+                    return -1
+                elif(x=='two_objects_both_identical'):
+                    inside_object_C = get_inside_object_two_obj_total(figures_C)
+                    outside_object_C = get_outside_object_two_obj_total(figures_C)
+
+                    print(inside_object_C)
+                    print(outside_object_C)
+
+                    answercnt=0
+                    for answer in potential_answers:
+                        answercnt+=1
+                        print(answercnt)
+                        choices_inside_object_answer = get_inside_object_two_obj_total(answer)
+                        choices_outside_object_answer = get_outside_object_two_obj_total(answer)
+
+                        choices_inside_obj_differences = compare_same_property_object(inside_object_C, choices_inside_object_answer)
+                        choices_outside_obj_differences = compare_same_property_object(outside_object_C, choices_outside_object_answer)
+
+                        if(choices_inside_obj_differences==False):
+                            continue
+                        choices_relevant_inside_obj_differences = select_relevant_transformation(choices_inside_obj_differences)
+                        choices_relevant_outside_obj_differences = select_relevant_transformation(choices_outside_obj_differences)
+
+                        print(choices_relevant_inside_obj_differences)
+                        print(choices_relevant_outside_obj_differences)
+
+                        if ((len(choices_relevant_outside_obj_differences) == 0) and len(choices_relevant_inside_obj_differences) == 1):
+                            print('')
+                            attribute_dif = relevant_inside_obj_differences[0]['differences']['attribute']
+                            # print(relevant_inside_obj_differences)
+                            print(attribute_dif)
+                            if (attribute_dif == 'inside'):
+                                returned_answer = int((answer[0]['figure']))
+                                return returned_answer
+
+                        # try:
+                        #     get_inside_object_two_obj_total(answer)
 
                     return -1
                 elif(x=='skip'):
@@ -270,18 +338,29 @@ class Agent:
 
                 # if both figures have one object inside of another
                 if(one_object_inside_another_A and one_object_inside_another_B):
-                    get_inside_object(figures_A)
-                    # inside_object_A
-                    # outside_object_A
-                    #
-                    # inside_object_B
-                    # outside_object_A
 
-                # for a, b in zip(figures_A, figures_B):
-                #     print(a)
-                #     print('_')
-                #     print(b)
-                #     print('\n')
+                    inside_object_A = get_inside_object_two_obj_total(figures_A)
+                    outside_object_A = get_outside_object_two_obj_total(figures_A)
+                    #
+                    inside_object_B = get_inside_object_two_obj_total(figures_B)
+                    outside_object_B = get_outside_object_two_obj_total(figures_B)
+
+                    #     Check which differences exist between
+                    inside_obj_differences = compare_same_property_object(inside_object_A,inside_object_B)
+                    outside_obj_differences = compare_same_property_object(outside_object_A, outside_object_B)
+
+                    relevant_inside_obj_differences = select_relevant_transformation(inside_obj_differences)
+                    relevant_outside_obj_differences = select_relevant_transformation(outside_obj_differences)
+
+                    if( (len(relevant_outside_obj_differences) ==0) and len(relevant_inside_obj_differences)==1 ):
+                        attribute_dif = relevant_inside_obj_differences[0]['differences']['attribute']
+
+                        # print(type(attribute_dif))
+                        if(attribute_dif=='inside'):
+                            # print('hell yes')
+                            match_goal = 'two_objects_both_identical'
+                            differences = relevant_inside_obj_differences
+                            print(differences)
 
 
                     # if(a['properties'])
